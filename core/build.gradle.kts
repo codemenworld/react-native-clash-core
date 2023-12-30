cb044e759005c4d4f0108d4ed3cf6cc0d688bcdf
helloworld-men <global.appcreater@gmail.com>
2023-12-30 16:09:41 +0800

    fix

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -27,10 +27,6 @@ val golangSource = file("src/main/golang/native")
 
 golang {
     sourceSets {
-        create("meta-alpha") {
-            tags.set(listOf("foss","with_gvisor"))
-            srcDir.set(file("src/foss/golang"))
-        }
         create("meta") {
             tags.set(listOf("foss","with_gvisor"))
             srcDir.set(file("src/foss/golang"))
